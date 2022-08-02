9cf0ea45278c5538e8665d06057f5735cd25c6d0
Nathan Clevenger <nateclev@gmail.com>
2022-08-02 01:30:11 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ export default {
   fetch: (req) => {
     const res = fetch('https://www.googleapis.com/robot/v1/metadata/jwk/securetoken@system.gserviceaccount.com', { cf: { cacheTtl: 20445,  cacheEverything: true }})
     const response = res.clone()
-    response.headers.set('Cache-Control', `max-age=20445, public`
+    response.headers.set('Cache-Control', `max-age=20445, public`)
     return response
   }
 }
