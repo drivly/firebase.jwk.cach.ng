abbc12694dd1e0ce8faca4b99bf64a418472ec13
Nathan Clevenger <nateclev@gmail.com>
2022-08-02 01:11:06 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,9 +2,9 @@ const interactionCounter = {}
 
 export default {
   fetch: (req) => {
-    const res = fetch('https://www.googleapis.com/robot/v1/metadata/jwk/securetoken@system.gserviceaccount.com', { cf: { cacheTtl: 24 * 60 * 60,  cacheEverything: true }}}
+    const res = fetch('https://www.googleapis.com/robot/v1/metadata/jwk/securetoken@system.gserviceaccount.com', { cf: { cacheTtl: 20445,  cacheEverything: true }}}
     const response = res.clone()
-    response.headers.set('Cache-Control', `max-age=${24 * 60 * 60}, public`
+    response.headers.set('Cache-Control', `max-age=20445, public`
     return response
   }
 }
